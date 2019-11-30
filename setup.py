b7324296ae7434b21fad9952d592853e8af66935
jkell17 <kelleher.jimmy@gmail.com>
2019-11-30 10:08:18 -0500

    Cleaned up // added mypy

--- setup.py
+++ setup.py
@@ -1,34 +1,23 @@
-from setuptools import setup
 import sys
-import os
-from typing import List
-assert sys.version_info >= (3, 7, 0), "Requires Python 3.7+"
 from pathlib import Path  # noqa E402
+from typing import List
+
+from setuptools import setup
+
+assert sys.version_info >= (3, 7, 0), "Requires Python 3.7+"
 
 CURRENT_DIR = Path(__file__).parent
 
-import codecs
-import os
-import re
-
-from setuptools import find_packages, setup
-
 
 NAME = "pyinit"
-VERSION = '0.0.1'
+VERSION = "0.0.1"
 
 
 def get_requirements() -> List[str]:
-    
-    with open(CURRENT_DIR / "requirements.txt") as f:
-        return f.read().split('\n')
 
+    with open(CURRENT_DIR / "requirements.txt") as f:
+        return f.read().split("\n")
 
 
 if __name__ == "__main__":
-    setup(
-        name=NAME,        
-        version=VERSION,
-        install_requires=get_requirements(),
-    )
-
+    setup(name=NAME, version=VERSION, install_requires=get_requirements())
